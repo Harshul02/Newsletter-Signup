6e79e473fd2d484bea5720282de8e8c6ef89a902
Harshul Jain <harshulgangwal02@gmail.com>
2022-08-15 16:32:02 +0530

    Added get and pust functions

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+// const request = require("request");
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+})
 
 
-
-
-
+app.post("/", function(req,res)
+{
+    const firstName= req.body.fName;
+    const lastName= req.body.lName;
+    const email= req.body.email;
+    console.log(firstName, lastName,  email);
+})
 
 
 
