973fa40e52f2b08410ef32dfc03bf2d2416a0a19
Harshul Jain <harshulgangwal02@gmail.com>
2022-08-15 17:15:57 +0530

    Data sending

--- app.js
+++ app.js
@@ -34,8 +34,27 @@ app.post("/", function(req,res)
     };
 
     const jsonData = JSON.stringify(data);
-    const url = "https://us18.api.mailchimp.com/3.0/lists";
-    https.request(options, (res) => {})
+    const url = "https://us18.api.mailchimp.com/3.0/lists/a0e480b91f";
+
+    const options = {
+        method: "POST",
+        auth: "Harshul:"
+    }
+
+    const request = https.request(url, options, function(response) {
+
+        if(response.statusCode === 200)
+        {
+            res.send("Successfully Registered");
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
         
 })
 
